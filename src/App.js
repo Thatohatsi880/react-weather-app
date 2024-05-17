2fbad965fd9a77ac453c12f6da073f758eda73a3
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-05-17 17:25:03 +0200

    Added Github repo Url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return( 
+  <div className="App">
+   <h1>Weather App</h1> 
+    <footer>
+      This project was coded by {" "}
+      <a href="https://dev.to/thatohatsi880" target="_blank" rel="noreferrer">
+        Thatohatsi Tilodi
+       
+      </a> {" "}
+      and is {" "}
+      <a href="https://github.com/Thatohatsi880/react-weather-app"
+      target="blank"
+      >
+        open-sourced on GitHub
+      </a>
+  </footer>
+</div>
+  )
 }
 
 
