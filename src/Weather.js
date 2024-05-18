9837fe9f475cece34e547d33cbf835fcc34fc095
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-05-18 10:19:54 +0200

    Added date on the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from 'axios';
 import "./Weather.css";
 
 export default function Weather(props){
     const[weatherData, setWeatherData] = useState({ready: false});
     function handleResponse(response){
-        console.log(response.data)
-    console.log(response.data);
     setWeatherData({
         ready: true,
         temperature: response.data.main.temp,
@@ -15,12 +14,12 @@ export default function Weather(props){
         description: response.data.weather[0].description,
         iconUrl:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII=',
         wind: response.data.wind.speed,
-        date: "Saturday 15:00",
+        date: new Date(response.data.dt* 1000),
         city: response.data.name
     });
    
 }
-
+// console.log(response.data);
 if (weatherData.ready){
     return(
         <div className="Weather">
@@ -45,9 +44,10 @@ if (weatherData.ready){
     </form>
       <h1>{weatherData.city}</h1>
       <ul>
-        <li>{weatherData.date}</li>
-        <li className="text-capitalize">
-            {weatherData.description}</li>
+        <li>
+            <FormattedDate date={weatherData.date}/>
+        </li>
+        <li className="text-capitalize">{weatherData.description}</li>
       </ul>
       
      <div className="row mt -6">
