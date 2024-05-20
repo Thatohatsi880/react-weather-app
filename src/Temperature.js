bd3ddc672b9a72e4b88636060f9ae451473cda27
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-05-20 21:04:12 +0200

    Refactored code and fixed warning

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  const celsius = props.celsius;
-  const fahrenheit = (celsius * 9) / 5 + 32;
-
-  const toggleUnit = () => {
-    setUnit(unit === "celsius" ? "fahrenheit" : "celsius");
-  };
-
   return (
     <div className="WeatherTemperature">
-      <span className="temperature">
-        {unit === "celsius" ? Math.round(celsius) : Math.round(fahrenheit)}
-      </span>
-      <span className="unit" onClick={toggleUnit}>
-        {unit === "celsius" ? "°C" : "°F"}
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">
+        °C 
       </span>
     </div>
   );
